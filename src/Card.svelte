d3bcf1b5ddddb2bdda066c48cbf14777335f7f35
Sebastian Gnat <upgradesebastian@gmail.com>
2022-09-21 04:24:21 +0200

    add gradient css style

--- src/Card.svelte
+++ src/Card.svelte
@@ -49,6 +49,22 @@
     overflow: hidden;
 
     user-select: none;
+
+/* background: rgba(255, 255, 255, 0.53);
+border-radius: 16px;
+box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+backdrop-filter: blur(5px);
+-webkit-backdrop-filter: blur(5px);
+border: 1px solid rgba(255, 255, 255, 0.3); */
+
+background: rgba( 255, 255, 255, 0.35 );
+box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+backdrop-filter: blur( 4px );
+-webkit-backdrop-filter: blur( 4px );
+border-radius: 10px;
+border: 1px solid rgba( 255, 255, 255, 0.18 );
+
+
   }
 </style>
 
